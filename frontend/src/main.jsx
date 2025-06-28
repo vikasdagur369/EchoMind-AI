8dcdb71ef7504f0e13b4fda2b03a969e60db614e
Vikas Dagur <108851539+vikasdagur369@users.noreply.github.com>
2025-06-28 09:59:34 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import UserContext from "./context/userContext.jsx";
+import userContext from "./context/userContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>

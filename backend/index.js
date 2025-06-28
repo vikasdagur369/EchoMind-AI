38b58e0ab11800c70cd2093ce0170fb0d085eecc
Vikas Dagur <108851539+vikasdagur369@users.noreply.github.com>
2025-06-28 10:23:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://echomind-ai-g6iv.onrender.com",
     credentials: true,
   })
 );

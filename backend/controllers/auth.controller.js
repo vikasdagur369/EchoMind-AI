69701431fdf72c1c0b4faceba25c285e7646a897
Vikas Dagur <108851539+vikasdagur369@users.noreply.github.com>
2025-06-28 10:24:27 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -28,8 +28,8 @@ export const signUp = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 1 * 24 * 60 * 60 * 1000,
-      sameSite: "strict",
-      secure: false,
+      sameSite: "none",
+      secure: true,
     });
 
     return res.status(201).json(user);
@@ -64,8 +64,8 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 1 * 24 * 60 * 60 * 1000,
-      sameSite: "strict",
-      secure: false,
+      sameSite: "none",
+      secure: true,
     });
 
     return res.status(200).json({ message: "user login successfully!" });
